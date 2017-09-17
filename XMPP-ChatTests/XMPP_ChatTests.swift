6d73de6b3bacc1b7d57c9a8fafb7ecba992b2b01
Sharad Paghadal <sharad.appdeveloper@gmail.com>
2017-09-17 19:33:46 +0530

    Initial Commit

---
+++ XMPP-ChatTests/XMPP_ChatTests.swift
@@ -0,0 +1,36 @@
+//
+//  XMPP_ChatTests.swift
+//  XMPP-ChatTests
+//
+//  Created by Sharad Paghadal on 17/09/17.
+//  Copyright © 2017 Sharad Paghadal. All rights reserved.
+//
+
+import XCTest
+@testable import XMPP_Chat
+
+class XMPP_ChatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
